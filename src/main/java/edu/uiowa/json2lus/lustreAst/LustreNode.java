96920b7ba7a08a9636ade948015505bf896c2b7f
Paul Meng <baolmeng@gmail.com>
2017-10-11 17:06:42 -0500

    Create lib node for abs, minmax. Refactor some codes

--- src/main/java/edu/uiowa/json2lus/lustreAst/LustreNode.java
+++ src/main/java/edu/uiowa/json2lus/lustreAst/LustreNode.java
@@ -5,6 +5,8 @@
  */
 package edu.uiowa.json2lus.lustreAst;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -12,21 +14,39 @@ import java.util.List;
  * @author Paul Meng
  */
 public class LustreNode extends LustreAst {
-	public final String             name;
-	public final List<LustreVar>    inputVars;
-	public final List<LustreVar>    outputVars;
-	public final List<LustreVar>    localVars;
-	public final List<LustreEq>     bodyExprs;
-        public final List<LustreEq>     propExprs;
-    
-        public LustreNode(String name, List<LustreVar> inputs, List<LustreVar> outputs, List<LustreVar> locals, List<LustreEq> bodyExprs, List<LustreEq> props) {
-            this.name       = name;
-            this.inputVars  = inputs;
-            this.outputVars = outputs;
-            this.localVars  = locals;
-            this.bodyExprs  = bodyExprs;
-            this.propExprs  = props;
-        }
+    public final String             name;
+    public final List<LustreVar>    inputVars;
+    public final List<LustreVar>    outputVars;
+    public final List<LustreVar>    localVars;
+    public final List<LustreEq>     bodyExprs;
+    public final List<LustreEq>     propExprs;
+
+    public LustreNode(String name, List<LustreVar> inputs, List<LustreVar> outputs, List<LustreVar> locals, List<LustreEq> bodyExprs, List<LustreEq> props) {
+        this.name       = name;
+        this.inputVars  = inputs;
+        this.outputVars = outputs;
+        this.localVars  = locals;
+        this.bodyExprs  = bodyExprs;
+        this.propExprs  = props;
+    }
+
+    public LustreNode(String name, List<LustreVar> inputs, List<LustreVar> outputs, List<LustreEq> bodyExprs) {
+        this.name       = name;
+        this.inputVars  = inputs;
+        this.outputVars = outputs;
+        this.bodyExprs  = bodyExprs;
+        this.localVars  = new ArrayList<>();
+        this.propExprs  = new ArrayList<>();            
+    }  
+
+    public LustreNode(String name, LustreVar input, LustreVar output, List<LustreEq> bodyExprs) {
+        this.name       = name;
+        this.inputVars  = Arrays.asList(input);
+        this.outputVars = Arrays.asList(output);
+        this.bodyExprs  = bodyExprs;
+        this.localVars  = new ArrayList<>();
+        this.propExprs  = new ArrayList<>();            
+    }          
 
     @Override
     public void accept(LustreAstVisitor visitor) {
